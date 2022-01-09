681d15aba6886116a928b53ff0c7691bc482f98d
Hoseok Na <hsna7024@gmail.com>
2022-01-09 19:45:33 +0900

    Feat: Create MessageInput

---
+++ mission/src/components/MessageInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input v-model="message" data-test="input" />
+    <p data-test="display">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessageInput',
+
+  data() {
+    return {
+      message: '',
+    };
+  },
+};
+</script>

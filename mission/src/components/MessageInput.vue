78a4648c49c65be03002d87b84e87cf110c4302b
Hoseok Na <hsna7024@gmail.com>
2022-01-09 19:55:45 +0900

    Feat: Add rotateMessage

--- mission/src/components/MessageInput.vue
+++ mission/src/components/MessageInput.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <input v-model="message" data-test="input" />
+    <button v-on:click="rotateMessage">회전</button>
     <p data-test="display">{{ message }}</p>
   </div>
 </template>
@@ -13,6 +14,12 @@ export default {
     return {
       message: '',
     };
+  },
+  methods: {
+    rotateMessage() {
+      const string = this.message.split('');
+      this.message = [...string.slice(1), string[0]].join('');
+    },
   },
 };
 </script>

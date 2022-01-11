5bde52c66adbec2d42008c7567fa0f6ba587676a
Hoseok Na <hsna7024@gmail.com>
2022-01-11 14:04:53 +0900

    Style: Lint code

--- mission/tests/unit/messageInput.spec.js
+++ mission/tests/unit/messageInput.spec.js
@@ -1,11 +1,11 @@
-import { shallowMount } from "@vue/test-utils";
-import MessageInput from "@/components/MessageInput.vue";
+import { shallowMount } from '@vue/test-utils';
+import MessageInput from '@/components/MessageInput.vue';
 
-describe("MessageInput", () => {
-  it("input창의 입력한 문자열과 display창의 문자열이 같아야 한다.", async () => {
+describe('MessageInput', () => {
+  it('input창의 입력한 문자열과 display창의 문자열이 같아야 한다.', async () => {
     const wrapper = shallowMount(MessageInput);
 
-    const TEST_MESSAGE = "사용자가 입력할거에요.";
+    const TEST_MESSAGE = '사용자가 입력할거에요.';
     await wrapper.find('[data-test="input"]').setValue(TEST_MESSAGE);
     const message = wrapper.get('[data-test="display"]');
 

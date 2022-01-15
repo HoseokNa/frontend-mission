d5ff76a4870ac8fb0337c140c7b06feb1743a88e
Hoseok Na <hsna7024@gmail.com>
2022-01-15 23:16:31 +0900

    Test: renders item image

--- mission/tests/unit/ItemInfo/ItemInfo.spec.js
+++ mission/tests/unit/ItemInfo/ItemInfo.spec.js
@@ -7,4 +7,11 @@ describe('ItemInfoPage', () => {
 
     expect(wrapper.find('#item-info-page').exists()).toBe(true);
   });
+
+  it('renders item image in image-container', () => {
+    const wrapper = mount(ItemInfoPage);
+    const imageContainer = wrapper.find('[data-test="item-image-container"]');
+
+    expect(imageContainer.find('[data-test="item-image-file"]').exists()).toBe(true);
+  });
 });

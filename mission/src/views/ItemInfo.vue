07ad6a29599712a25f2da7457ea86294d4a18abb
Hoseok Na <hsna7024@gmail.com>
2022-01-15 23:55:34 +0900

    Feat: Add item-image-container

--- mission/src/views/ItemInfo.vue
+++ mission/src/views/ItemInfo.vue
@@ -1,24 +1,20 @@
 <template>
-<div id='item-info-page'></div>
+  <div id="item-info-page">
+    <div data-test="item-image-container">
+      <img data-test="item-image-file" src="@/assets/item_sample_image.jpeg" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ItemInfoPage',
   data() {
-    return {
-
-    };
+    return {};
   },
-  methods: {
-
-  },
-  computed: {
-
-  },
+  methods: {},
+  computed: {},
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
